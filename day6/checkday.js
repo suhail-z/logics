7cc1be28c94020bd8784f4e76b21ead90fb9c925
suhail-z <suhail22rm@gmail.com>
2024-09-20 00:35:36 +0530

    upd

---
+++ day6/checkday.js
@@ -0,0 +1,8 @@
+let days=['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+if(days.includes("sunday"))
+    console.log(days.indexOf("sunday"));
+else
+console.log('doesn-t exist');
+
+    
+    
